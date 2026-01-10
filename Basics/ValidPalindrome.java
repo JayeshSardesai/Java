d1d844348c093360eede770491d196dcedab005f
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-10 21:13:28 +0530

    Add ValidPalindrome class to check palindromes

---
+++ Basics/ValidPalindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean pali(String s,int start,int end){
+        if(start>=end){
+            return true;
+        }
+        if(s.charAt(start)!=s.charAt(end)){
+            return false;
+        }
+        return pali(s,start+1,end-1);
+    }
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        s=s.toLowerCase();
+        return pali(s,0,s.length()-1);
+    }
+}
