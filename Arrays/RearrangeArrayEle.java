6104786a3bcd0ce2822e4ac2ed84a9eeb1772fd7
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-10 21:36:07 +0530

    Add rearrangeArray method for alternating positives and negatives
    Implemented a method to rearrange an array such that positive and negative numbers alternate.

---
+++ Arrays/RearrangeArrayEle.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int positive=0,negative=1;
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                arr[positive]=nums[i];
+                positive+=2;
+            }
+            if(nums[i]<0){
+                arr[negative]=nums[i];
+                negative+=2;
+            }
+        }
+        return arr;
+    }
+}
