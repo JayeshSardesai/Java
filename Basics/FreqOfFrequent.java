a3b64baab75c57083480c7c71b2894b9c663ad08
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-10 21:15:11 +0530

    Implement maxFrequency method in FreqOfFrequent class

---
+++ Basics/FreqOfFrequent.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int freq1=1;
+        int freq2=0;
+        int i=0;
+        int max=1;
+        int l=k;
+        while(i<arr.length{
+            if(max<)
+            int j=0;
+            while(j<k){
+                if(nums.contains(nums[i]+(j+1))){
+                    max++;
+
+                    break;
+                }
+            }
+            i++;
+        }
+    }
+}
