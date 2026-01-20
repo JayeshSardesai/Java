127d8fe3ea6ebfcb407038b2a9140447d7227ce8
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-20 21:08:36 +0530

    Create flatteningLL.java

---
+++ LinkedList/flatteningLL.java
@@ -0,0 +1,31 @@
+class Solution {
+    // Function to flatten a linked list
+    Node merge(Node list1,Node list2){
+        Node dummy=new Node(-1);
+        Node res=dummy;
+        while(list1!=null&&list2!=null){
+            if(list1.data<list2.data){
+                res.bottom=list1;
+                res=res.bottom;
+                list1=list1.bottom;
+            }else{
+                res.bottom=list2;
+                res=res.bottom;
+                list2=list2.bottom;
+            }
+        }
+        if(list1!=null){
+            res.bottom=list1;
+        }
+        if(list2!=null){
+            res.bottom=list2;
+        }
+        return dummy.bottom;
+    }
+    Node flatten(Node root) {
+        // code here
+        if(root==null||root.next==null) return root;
+        Node mergedHead=flatten(root.next);
+        return merge(root,mergedHead);
+    }
+}
