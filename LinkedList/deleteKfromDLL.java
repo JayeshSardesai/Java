f6f11a6cfe9f0a250d9068c5a00fbab42fed5f00
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-20 21:04:53 +0530

    Create deleteKfromDLL.java

---
+++ LinkedList/deleteKfromDLL.java
@@ -0,0 +1,36 @@
+// User function Template for Java
+
+/* Structure of Doubly Linked List
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}*/
+class Solution {
+    static Node deleteAllOccurOfX(Node head, int x) {
+        // Write your code here
+        if(head==null) return head;
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data==x){
+                if(temp==head){
+                    head=temp.next;
+                    if(head!=null){
+                        head.prev=null;
+                    }
+                }else{
+                    temp.prev.next=temp.next;
+                    if(temp.next!=null) temp.next.prev=temp.prev; 
+                }
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
