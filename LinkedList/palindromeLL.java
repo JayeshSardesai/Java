c121991c89c4d779e16b1d74ae1531c6d88163df
Jayesh Sardesai <desaijayash@gmail.com>
2026-01-20 20:57:23 +0530

    Create palindromeLL.java

---
+++ LinkedList/palindromeLL.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode newHead=reverse(slow.next);
+        ListNode first=head;
+        ListNode sec=newHead;
+        while(sec!=null){
+            if(first.val!=sec.val){
+                reverse(newHead);
+                return false;
+            }
+            first=first.next;
+            sec=sec.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head){
+        if(head==null||head.next==null) return head;
+        ListNode newHead=reverse(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return newHead;
+    }
+}
