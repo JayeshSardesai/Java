9c77a62a099545d9f106ee6f2cb0b891cc6ec2f1
Jayesh Sardesai <desaijayash@gmail.com>
2026-02-11 19:39:36 +0530

    Add BST recovery algorithm in recoverBST.java
    Implement a solution to recover a binary search tree by swapping the incorrect nodes.

---
+++ BinarySearchTree/recoverBST.java
@@ -0,0 +1,44 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    Node first,last,middle,prev;
+    public void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        if(prev!=null&&root.data<prev.data){
+            if(first==null){
+                first=prev;
+                middle=root;
+            }else last=root;
+        }
+        prev=root;
+        inorder(root.right);
+    }
+    void correctBST(Node root) {
+        // code here.
+        first=middle=last=null;
+        prev=new Node(Integer.MIN_VALUE);
+        inorder(root);
+        if(first!=null&&last!=null){
+            int v=first.data;
+            first.data=last.data;
+            last.data=v;
+        }else if(first!=null&&middle!=null){
+            int v=first.data;
+            first.data=middle.data;
+            middle.data=v;
+        }
+    }
+}
