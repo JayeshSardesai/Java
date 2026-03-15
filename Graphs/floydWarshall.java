de6c58929ac86b9c5d19ea4d30784f509bcd3c74
Jayesh Sardesai <desaijayash@gmail.com>
2026-03-15 20:53:03 +0530

    Create floydWarshall.java

---
+++ Graphs/floydWarshall.java
@@ -0,0 +1,16 @@
+// User function template for JAVA
+
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        // Code here
+        int n=dist.length;
+        int max=100000000;
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k]!=max&&dist[k][j]!=max) dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+}
