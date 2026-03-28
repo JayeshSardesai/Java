f5aa3ce7606e5a9683a4df3bb798fff98562746b
Jayesh Sardesai <desaijayash@gmail.com>
2026-03-28 19:58:41 +0530

    Update fibonacci.java

--- DP/fibonacci.java
+++ DP/fibonacci.java
@@ -1,3 +1,4 @@
+// Using the DP memoisation.
 class Solution {
     public int nthFibonacci(int n) {
         // code here
@@ -9,5 +10,21 @@ class Solution {
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+    }
+}
+
+// Using the tabulation but optimised one for that 
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n==0) return 0;
+        int fir=0,sec=1;
+        int th=fir+sec;
+        for(int i=3;i<=n;i++){
+            fir=sec;
+            sec=th;
+            th=fir+sec;
+        }
+        return th;
     }
 }
