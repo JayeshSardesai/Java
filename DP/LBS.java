ec16b97bf3df3abf5a0e54b86c8b31fae13850be
Jayesh Sardesai <desaijayash@gmail.com>
2026-05-10 18:47:53 +0530

    Implement longest bitonic sequence function

---
+++ DP/LBS.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int longestBitonicSequence(int n, int[] nums) {
+        // code here
+        int[] dp1=new int[n+1];
+        Arrays.fill(dp1,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp1[i]=Math.max(dp1[i],1+dp1[j]);
+                }
+            }
+        }
+        int[] dp2=new int[n+1];
+        Arrays.fill(dp2,1);
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[i]>nums[j]){
+                    dp2[i]=Math.max(dp2[i],1+dp2[j]);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp1[i]>1&&dp2[i]>1) ans=Math.max(ans,dp1[i]+dp2[i]-1);
+        }
+        return ans;
+    }
+}
