b692540d0a0e42edab78cba5d205ad693aadb4f7
Jayesh Sardesai <desaijayash@gmail.com>
2026-05-12 23:16:31 +0530

    Implement matrix multiplication using dynamic programming

---
+++ DP/MCM.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int matrixMultiplication(int arr[]) {
+        // code here
+        int n=arr.length;
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        return helper(1,n-1,arr,dp);
+    }
+    static int helper(int i,int j,int[] arr,int[][] dp){
+        if(i==j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int min=(int)1e9;
+        for(int k=i;k<j;k++){
+            int step=arr[i-1]*arr[k]*arr[j]+helper(i,k,arr,dp)+helper(k+1,j,arr,dp);
+            min=Math.min(step,min);
+        }
+        return dp[i][j]=min;
+    }
+}
