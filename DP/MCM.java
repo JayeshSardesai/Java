8ba4c272d97ea92ffe1c12a168fd76e7c6d1a1cc
Jayesh Sardesai <desaijayash@gmail.com>
2026-05-13 22:48:17 +0530

    Implement matrix multiplication using tabulation and memoization

--- DP/MCM.java
+++ DP/MCM.java
@@ -1,3 +1,24 @@
+// Tabulation Method.
+class Solution {
+    static int matrixMultiplication(int arr[]) {
+        // code here
+        int n=arr.length;
+        long[][] dp=new long[n][n];
+        for(int i=n-1;i>=1;i--){
+            for(int j=i+1;j<n;j++){
+                long min=Long.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    long step=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    min=Math.min(step,min);
+                }
+                dp[i][j]=min;
+            }
+        }
+        return (int)dp[1][n-1];
+    }
+}
+
+// Memoisation Method.
 class Solution {
     static int matrixMultiplication(int arr[]) {
         // code here
