44c075b7de089eacff8b333e62d7b6a93a3fac50
Jayesh Sardesai <desaijayash@gmail.com>
2026-05-28 20:07:14 +0530

    Create minParenthesis.java

---
+++ Strings/minParenthesis.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minParentheses(String s) {
+        // code here
+        int openCnt=0;
+        int closeCnt=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                openCnt++;
+            }else{
+                if(openCnt>0){
+                    openCnt--;
+                }else{
+                    closeCnt++;
+                }
+            }
+        }
+        return openCnt+closeCnt;
+    }
+}
