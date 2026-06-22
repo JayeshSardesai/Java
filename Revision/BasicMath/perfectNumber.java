2edd9262822675f2949dbafae926bda5b78b056d
Jayesh Sardesai <desaijayash@gmail.com>
2026-06-22 20:33:47 +0530

    Create perfectNumber.java

---
+++ Revision/BasicMath/perfectNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                if(i!=num) sum+=i;
+                if(num/i!=i&&num/i!=num) sum+=(num/i);
+            }
+        }
+        return sum==num;
+    }
+}
