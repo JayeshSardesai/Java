6457e11af3225d7a696042c012ed505d600eef25
Jayesh Sardesai <desaijayash@gmail.com>
2026-07-14 21:19:13 +0530

    Update unionOfArrays.java

--- Revision/Array/unionOfArrays.java
+++ Revision/Array/unionOfArrays.java
@@ -1,3 +1,4 @@
+// Here we are trying to skip the duplicates using while loop inside the main loop.
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // code here
@@ -29,6 +30,47 @@ class Solution {
         while(j < m) {
             if (j == 0 || b[j] != b[j - 1]){
                 res.add(b[j]);
+            }
+            j++;
+        }
+        return res;
+    }
+}
+
+// Here we are ignoring the element which is equal to last element of list.
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // code here
+        int i = 0;
+        int j = 0;
+        int n = a.length;
+        int m = b.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        if(a[i] < b[j]) res.add(a[i++]);
+        else res.add(b[j++]);
+        int prev = res.get(0);
+        while(i < n && j < m) {
+            if (a[i] < b[j]){
+                if (a[i] != prev) res.add(prev = a[i]);
+                i++;
+            } else if(a[i] > b[j]) {
+                if(b[j] != prev) res.add(prev = b[j]);
+                j++;
+            } else{
+                if (a[i] != prev) res.add(prev = a[i]);
+                i++;
+                j++;
+            }
+        }
+        while(i < n) {
+            if (a[i] != prev){
+                res.add(prev = a[i]);
+            }
+            i++;
+        }
+        while(j < m) {
+            if (b[j] != prev){
+                res.add(prev = b[j]);
             }
             j++;
         }
