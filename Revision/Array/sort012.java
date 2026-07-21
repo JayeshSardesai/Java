cf676ddbd2bce31708f68500b846efb0b1e2883a
Jayesh Sardesai <desaijayash@gmail.com>
2026-07-21 21:12:07 +0530

    Implement Dutch National Flag algorithm for sorting

---
+++ Revision/Array/sort012.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start = 0, mid = 0, end = nums.length - 1;
+        while (mid <= end) {
+            if (nums[mid] == 0) {
+                nums[mid] = (nums[mid] + nums[start]) - (nums[start] = nums[mid]);
+                start++;
+                mid++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else if (nums[mid] == 2) {
+                nums[mid] = (nums[mid] + nums[end]) - (nums[end] = nums[mid]);
+                end--;
+            }
+        }
+    }
+}
