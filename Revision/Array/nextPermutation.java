d628056d4eb5f4fd6eca87673155b779a392b5cb
Jayesh Sardesai <desaijayash@gmail.com>
2026-08-03 21:22:42 +0530

    Implement nextPermutation method in Solution class

---
+++ Revision/Array/nextPermutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 1;
+        int j = 0;
+        int change = -1;
+        while (i >= 0) {
+            j = n - 1;
+            while (j > i) {
+                if (nums[i] < nums[j]) {
+                    change = j;
+                    break;
+                }
+                j--;
+            }
+            if (change != -1) {
+                nums[i] =  nums[i] + nums[change] - (nums[change] = nums[i]);
+                break;
+            }
+            i--;
+        }
+        if (change == -1) {
+            i = 0;
+            while (i < n/2) {
+                nums[i] = nums[i] + nums[n - i - 1] - (nums[n - i - 1] = nums[i]);
+                i++;
+            }
+        } else {
+            i = i + 1;
+            j = n - 1;
+            while (i < j) {
+                nums[i] = nums[i] + nums[j] - (nums[j] = nums[i]);
+                i++;
+                j--;
+            }
+        }
+    }
+}
