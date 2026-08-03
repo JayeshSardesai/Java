0708a001d9ecf14170194c27055816762449f189
Jayesh Sardesai <desaijayash@gmail.com>
2026-08-03 21:24:40 +0530

    Refactor nextPermutation logic for clarity

--- Revision/Array/nextPermutation.java
+++ Revision/Array/nextPermutation.java
@@ -21,18 +21,14 @@ class Solution {
         }
         if (change == -1) {
             i = 0;
-            while (i < n/2) {
-                nums[i] = nums[i] + nums[n - i - 1] - (nums[n - i - 1] = nums[i]);
-                i++;
-            }
         } else {
             i = i + 1;
-            j = n - 1;
-            while (i < j) {
-                nums[i] = nums[i] + nums[j] - (nums[j] = nums[i]);
-                i++;
-                j--;
-            }
+        }
+        j = n - 1;
+        while (i < j) {
+            nums[i] = nums[i] + nums[j] - (nums[j] = nums[i]);
+            i++;
+            j--;
         }
     }
 }
