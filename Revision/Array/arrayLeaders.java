00d902249c23b92e2bc5e039f9ad4d68ea4e583f
Jayesh Sardesai <desaijayash@gmail.com>
2026-08-04 20:16:43 +0530

    Add leaders function in arrayLeaders.java
    Implement leaders function to find leader elements in an array.

---
+++ Revision/Array/arrayLeaders.java
@@ -0,0 +1,17 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        int n = arr.length;
+        int max = arr[n - 1];
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(max);
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                ans.add(max);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
